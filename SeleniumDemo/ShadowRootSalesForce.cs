922755c6957c150b8fc817290c708e667179ac3c
Prasath JP <prasath_dhan@yahoo.co.in>
2025-07-19 14:12:52 +0530

    Removed unused Usings in ShadowRootSalesForce.cs file

--- SeleniumDemo/ShadowRootSalesForce.cs
+++ SeleniumDemo/ShadowRootSalesForce.cs
@@ -1,12 +1,7 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.Support.Extensions;
 using OpenQA.Selenium.Support.UI;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SeleniumDemo
 {
